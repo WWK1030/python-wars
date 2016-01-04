9dea1a8d33b07b65a7d0c9bfccad2be88de70c3e
yann <yann@yanndeMacBook-Pro-2.local>
2016-01-04 11:12:12 +0800

    fix bug

--- xiayan/w1.py
+++ xiayan/w1.py
@@ -12,25 +12,28 @@ def count_for_loan(url: str):
     return result.headers.get('X-Record-Count')
 
 
-def list_of_car_loan(url, start, size):
-    params = {"from": start, "size": size}
+def list_of_loan(url, start, size):
+    params = {"from": start, "size": size, "productType": "CAR_LOAN_REQUEST"}
     result = requests.get(url=url, params=params)
     return result.json()
 
 
 def sum_of_loan(url: str):
-    count_car_loan = int(count_for_loan(url))
-    i, sum_of_car_money = 0, 0
-    while i <= count_car_loan - 10:
-        all_car_loan = list_of_car_loan(url, i, 10)
+    count_loan = int(count_for_loan(url))
+    i, sum_of__money = 0, 0
+    while i <= count_loan:
+        all_car_loan = list_of_loan(url, i, 10)
         i += 10
         for car_loan in all_car_loan:
-            sum_of_car_money += car_loan['currentInvestmentAmount']
-    return sum_of_car_money
+            sum_of__money += car_loan['currentInvestmentAmount']
+    return sum_of__money
 
+
+sum_of_car = sum_of_loan(CAR_LOAN_URL)
+print('car loan %s' % sum_of_car)
+
+sum_of_enterprise = sum_of_loan(ENTERPRISE_LOAN_URL)
+print('enterprise loan %s' % sum_of_enterprise)
 
 sum_of_teng = sum_of_loan(TENG_LOAN_URL)
-sum_of_car = sum_of_loan(CAR_LOAN_URL)
-sum_of_enterprise = sum_of_loan(ENTERPRISE_LOAN_URL)
-
-print('car loan %s, enterprise loan %s, tengxin %s' % (sum_of_car, sum_of_enterprise, sum_of_teng))
+print('tengxin %s' % sum_of_teng)
